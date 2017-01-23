43c7c255aa2547c4270b7fdea6dabde809a563a3
Tory Herman <tory.herman12@gmail.com>
2017-01-22 19:38:16 -0600

    scope posts to users

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,16 @@
 require 'random_data'
 
+# Create Users
+5.times do
+  User.create!(
+    name: RandomData.random_name,
+    email: RandomData.random_email,
+    password: RandomData.random_sentence
+  )
+end
+
+users = User.all
+
 # Create Topics
 15.times do
   Topic.create!(
@@ -13,6 +24,7 @@ topics = Topic.all
 # Create Posts
 50.times do
   Post.create!(
+    user: users.sample,
     topic: topics.sample,
     title: RandomData.random_sentence,
     body: RandomData.random_paragraph
@@ -29,7 +41,15 @@ posts = Post.all
   )
 end
 
+user = User.first
+user.update_attributes!(
+  name: 'Tory Herman',
+  email: 'tory.herman12@gmail.com',
+  password: 'password'
+)
+
 puts "Seed finished"
+puts "#{User.count} users created"
 puts "#{Topic.count} topics created"
 puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
