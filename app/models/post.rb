6029609c2425e169069d8021e39d9ded0e4d63d7
Tory Herman <tory.herman12@gmail.com>
2017-01-22 20:01:09 -0600

    add additional scopes

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,7 +3,9 @@ class Post < ApplicationRecord
   belongs_to :user
   has_many :comments, dependent: :destroy
 
-  default_scope { order('created_at DESC') }
+  default_scope { order(created_at: :desc) }
+  scope :ordered_by_title, -> { order(title: :asc) }
+  scope :ordered_by_reverse_created_at, -> { order(created_at: :asc) }
 
   validates :title, length: { minimum: 5 }, presence: true
   validates :body, length: { minimum: 20 }, presence: true
