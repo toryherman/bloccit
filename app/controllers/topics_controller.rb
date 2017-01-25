0b62ab6d841abc629471c245654ed33cd5538680
Tory Herman <tory.herman12@gmail.com>
2017-01-24 19:42:48 -0600

    add moderator role

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,6 +1,7 @@
 class TopicsController < ApplicationController
   before_action :require_sign_in, except: [:index, :show]
-  before_action :authorize_user, except: [:index, :show]
+  before_action :authorize_user, only: [:new, :create, :destroy]
+  before_action :authorize_moderator, only: [:edit, :update]
 
   def index
     @topics = Topic.all
@@ -62,6 +63,13 @@ class TopicsController < ApplicationController
   def authorize_user
     unless current_user.admin?
       flash[:alert] = "You must be an admin to do that."
+      redirect_to topics_path
+    end
+  end
+
+  def authorize_moderator
+    unless current_user.admin? || current_user.moderator?
+      flash[:alert] = "You must be an admin or moderator to do that."
       redirect_to topics_path
     end
   end
