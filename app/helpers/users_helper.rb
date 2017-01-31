677062a22ee10b293ab7cae19e1e3a9450631cda
Tory Herman <tory.herman12@gmail.com>
2017-01-30 18:53:28 -0600

    add favorites to user show view

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,16 @@
 module UsersHelper
+  def has_posted?
+    @user = User.find(params[:id])
+    @user.posts.length > 0
+  end
+
+  def has_commented?
+    @user = User.find(params[:id])
+    @user.comments.length > 0
+  end
+
+  def has_favorited?
+    @user = User.find(params[:id])
+    @user.favorites.length > 0
+  end
 end
